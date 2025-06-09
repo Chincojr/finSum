336cc130d3249e3adf67abcac5b6d86bd733b996
LightsidE <ojeleyeprecious@gmail.com>
2025-06-09 22:17:18 +0100

    more changes to dashboard page

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from "react";
 import Sidebar from "../component/Sidebar";
 import Navbar from "../component/Navbar";
 import avatar from "../Assests/Rectangle.svg";
-import cup from "../Assests/icons/cup2.svg"
+import cup from "../Assests/icons/cup2.svg";
+import TeslaLogo from "../Assests/icons/tesla-motors-1 1.svg"
+import graph from "../Assests/icons/Group 3.svg"
 
 function DashboardPage() {
     const [data, setData] = useState(null);
@@ -25,7 +27,10 @@ function DashboardPage() {
                     name: "Tesla Inc.",
                     invested: "8,000",
                     current: "43,000",
+                    abbrevation:"TSLA",
                     growth: "+17.63",
+                    logo: <img src={TeslaLogo} alt="logo"/>,
+                    graph: <img src={graph} alt="graph"/>
                 },
                 {
                     name: "Nasdaq",
@@ -56,9 +61,9 @@ function DashboardPage() {
             <Navbar />
             <main className="flex-1 bg-[#f8f9fc] px-6 py-4 overflow-y-auto">
                 <p className="pb-4 font-poppins text-sm text-gray-400 font-normal">Persona Overview</p>
-                <div className="flex gap-6 mb-10">
+                <div className="flex gap-6 mb-10 w-[100%]">
                 {/* Persona Overview */}
-                <div className="bg-white rounded-xl col-span-1 pr-12 pl-12 py-6 min-w-[330px] flex flex-col items-center justify-between text-center min-h-[300px]">
+                <div className="bg-white rounded-xl col-span-1 pr-12 pl-12 py-6 w-[28%] flex flex-col items-center justify-between text-center min-h-[300px]">
                     <div className="flex justify-between w-full">
                         <div>
                             <p className="text-gray-400 font-poppins font-normal text-base">Age</p>
@@ -106,12 +111,30 @@ function DashboardPage() {
                     </p>
 
                     <div className="mt-6">
-                        <p className="font-medium mb-2">My Stock</p>
+                        <p className="font-normal mb-2">My Stock</p>
                         {data.stocks.map((stock, index) => (
                         <div key={index} className="mb-3">
-                            <p className="text-sm font-semibold">{stock.name}</p>
-                            <p className="text-xs text-gray-500">Invested: NGN {stock.invested} | Current: NGN {stock.current}</p>
-                            <p className="text-xs text-green-600">{stock.growth}</p>
+                            <div className="flex justify-between items-center px-3">
+                            <div className="flex items-center">
+                                <div>{stock.logo}</div>
+                                <p className="text-sm font-semibold">{stock.name}</p>                                
+                            </div>
+                            <div>
+                                <p className="">{stock.abbrevation}</p>
+                                <p className="text-xs text-green-600">{stock.growth}</p>
+                            </div>
+                            </div>
+                            <div className="flex justify-between items-center mt-5">
+                                <div>
+                                    <p className="text-xs text-gray-500">Invested Value</p>
+                                    <p>NGN {stock.invested}  </p>
+                                </div>
+                                <div>
+                                    <p className="text-xs text-gray-500">Current Value</p>
+                                    <p>NGN {stock.current}</p>
+                                </div>
+                                <div>{stock.graph}</div>
+                            </div>
                         </div>
                         ))}
                     </div>
